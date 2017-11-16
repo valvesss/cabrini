e3bd39972be28a355c183c920a127f2f802b6c75
valvesss <vitoralvesinfo@gmail.com>
2017-11-16 10:34:09 -0200

    Update osmany.sh

--- osmany.sh
+++ osmany.sh
@@ -8,21 +8,16 @@ MAIN() {
 
 ## Mensagem de apresentação do script.
 
-	echo "Bem vindo ao Gerenciador de Sistema OSM!"
-	echo
-
-## Apresenta as opções ao usuário.
-
-	echo " 1. Gerenciador de Disco "
-	echo " 2. Gerenciiador de Memória "
-	echo " 3. Gerenciador de Processos "
-	echo " 4. Créditos "
-	echo " 5. Sair. "
-	echo
-
-## Lê a opção do usuário.
-
-	read -e -p $'Escolha a opção desejada: ' opt 
+opt=$(dialog					\
+	--stdout				\
+	--title 'Gerenciador de Sistema OSM'	\
+	--menu 'Escolha a opção desejada:'	\
+	0 0 0					\
+	1 'Gerenciador de Disco'		\
+	2 'Gerenciiador de Memória'		\
+	3 'Gerenciador de Processos'		\
+	4 'Créditos'				\
+	5 'Sair.')
 
 ## Invoca a opção selecionada do usuário.
 
@@ -31,8 +26,7 @@ MAIN() {
 		2) germem ;;
 		3) gerpro ;;
 		4) creditos ;;
-		5) echo "Saindo..." ; exit ;; 
-		*) echo "Opção inválida, tente novamente." ; sleep 2 ; MAIN ;;
+		5) exit ;; 
 	esac
 
 }
@@ -40,16 +34,14 @@ MAIN() {
 ## Função para facilitar retorno ao menu principal
 function goback(){
 
-	echo
-	read -e -p $'Deseja retornar ao menu inicial? [s/n]: ' opt
 
-	if [ $opt == 's' ]; then
+	dialog --stdout --yesno 'Deseja retornar ao menu inicial?' 0 0
+
+	if [ $? = 0 ]; then
 		MAIN
-	elif [ $opt == 'n' ]; then
+	else
+		clear
 		exit
-        else
- 		echo "Opção inválida, tente novamente."
-		goback
 	fi
 }
 
@@ -58,37 +50,28 @@ function gerdis() {
 
 ## Mensagem de apresentação.
 
-	clear
-
-	echo "Bem vindo ao Gerenciador de Disco do OSM!"
-	echo
-	echo " 1. Informações sobre espaço em disco "
-	echo " 2. Informações sobre partições"
-	echo " 3. Voltar"
-	echo " 4. Sair"
-	echo
-	
-	read -e -p $'Escolha a opção desejada: ' opt
+opt=$(dialog							\
+	--stdout						\
+	--title 'Bem vindo ao Gerenciador de Disco do OSM!'	\
+	--menu 'Escolha a opção desejada:'			\
+	0 0 0							\
+	1 'Informações sobre espaço em disco'			\
+	2 'Informações sobre partições'				\
+	3 'Voltar'						\
+	4 'Sair')
 	
 	case $opt in
 		1) subgerdis1 ;;
 		2) subgerdis2 ;;
 		3) MAIN ;;
-		4) echo "Saindo..." ; exit ;;
-		*) echo "Opção inválida, tente novamente." ; sleep 2 ; gerdis ;;
+		4) exit ;;
 	esac
 }
 ## Subfunção 1 (Gerenciamento de Disco): informações sobre espaço em disco
 	function subgerdis1() {
-	echo
-	echo "Você escolheu 'Informações sobre espaço em disco': " 
-	echo
 
-## Comando
-
-	df -hT
-
-## Pergunta ao usuário se quer voltar ou sair.
+	df -hT > /tmp/disco.txt
+	dialog --title 'Informações sobre espaço em disco' --textbox /tmp/disco.txt 0 0 \
 
 	goback
 
@@ -97,15 +80,9 @@ function gerdis() {
 ## Subfunção 2 (Gerenciamento de Disco): informações sobre partições
 
 	function subgerdis2() {
-	echo
-	echo "Você escolheu 'Informações sobre partições': "
-	echo
 
-## Comando
-
-	fdisk -l
-
-## Pergunta ao usuário se quer voltar ou sair.
+	fdisk -l > /tmp/disco1.txt
+	dialog --title 'Informações sobre as partições' --textbox /tmp/disco1.txt 0 0\
 
 	goback
 
@@ -116,55 +93,27 @@ function germem(){
 
 ## Mensagem de apresentação.
 
-	clear
-
-	echo "Bem vindo ao Gerenciador de Memória do OSM!"
-	echo
-	echo " 1. Informações sobre memória livre "
-	echo " 2. Informações sobre uso da memória"
-	echo " 3. Voltar"
-	echo " 4. Sair"
-	echo
-	
-	read -e -p $'Escolha a opção desejada: ' opt
+opt=$(dialog							\
+	--stdout						\
+	--title 'Bem vindo ao Gerenciador de Memória do OSM!'	\
+	--menu 'Escolha a opção desejada:'			\
+	0 0 0							\
+	1 'Informações sobre a memória'				\
+	2 'Voltar'						\
+	3 'Sair')
 	
 	case $opt in
 		1) subgermem1 ;;
-		2) subgermem2 ;;
-		3) MAIN ;;
-		4) echo "Saindo..." ; exit ;;
-		*) echo "Opção inválida, tente novamente." ; sleep 2 ; gerdis ;;
+		2) MAIN ;;
+		3) exit ;;
 	esac
 }
 
 ## Subfunção 1 (Gerenciamento de Memória): informações sobre memória livre
 	function subgermem1() {
-	echo
-	echo "Você escolheu 'Informações sobre memória livre': " 
-	echo
 
-## Comando
-
-	free
-
-## Pergunta ao usuário se quer voltar ou sair.
-
-	goback
-
-	}
-
-## Subfunção 2 (Gerenciamento de Memória): informações sobre uso da memória
-
-	function subgermem2() {
-	echo
-	echo "Você escolheu 'Informações sobre uso da memória': "
-	echo
-
-## Comando
-
-	fdisk -l
-
-## Pergunta ao usuário se quer voltar ou sair.
+	export NMON=m
+	nmon
 
 	goback
 
@@ -175,18 +124,16 @@ function gerpro(){
 
 ## Mensagem de apresentação.
 
-	clear
-
-	echo "Bem vindo ao Gerenciador de Memória do OSM!"
-	echo
-	echo " 1. Informações sobre os processos em tempo real"
-	echo " 2. Snapshot sobre os processos atuais"
-	echo " 3. Encerrar algum processo"
-	echo " 4. Voltar"
-	echo " 5. Sair"
-	echo
-	
-	read -e -p $'Escolha a opção desejada: ' opt
+opt=$(dialog							\
+	--stdout						\
+	--title 'Bem vindo ao Gerenciador de Processos do OSM!'	\
+	--menu 'Escolha a opção desejada:'			\
+	0 0 0							\
+	1 'Informações sobre os processos em tempo real'	\
+	2 'Snapshot sobre os processos atuais'			\
+	3 'Encerrar algum processo'				\
+	4 'Voltar'						\
+	5 'Sair')
 	
 	case $opt in
 		1) subgerpro1 ;;
@@ -289,11 +236,11 @@ function gerpro(){
 	}
 function creditos() {
 
-echo Jhonny 
-echo Papa
-echo Pukas
-echo Valves
-echo Manoemano
+dialog 						\
+	--title 'Integrantes'			\ 
+	--msgbox 'Jhonny Papa Pukas Valves' 	\
+	0 0
+
 goback
 
 }
