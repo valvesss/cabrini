3f89751aa6c84ac55bd9ce76e632193b7e78880f
valvesss <vitoralvesinfo@gmail.com>
2017-11-20 00:22:12 -0200

    Update osmany.sh
    Adicionada a opção de monitorar o sistema.

--- osmany.sh
+++ osmany.sh
@@ -18,6 +18,10 @@
 
 		if [[ $b -eq 1 ]]; then
 			echo "Para executar o script é necessário instalar as dependências mostradas acima."
+			read -e -p $'Deseja instalá-las? [s/n]' opt
+				if [[ $opt = 's' ]] || [[ $opt = 'S' ]]; then
+					apt install -y dialog nmon
+				fi
 			exit
 		fi
 
@@ -38,7 +42,7 @@ function leave(){
 	exit
 }
 
-MAIN() {
+function main() {
 
 ## Limpa a tela 
 
@@ -54,18 +58,17 @@ opt=$(dialog					\
 	1 'Gerenciador de Disco'		\
 	2 'Gerenciador de Memória'		\
 	3 'Gerenciador de Processos'		\
-	4 'Sair.')
+	4 'Monitorar Sistema'			\
+	5 'Sair.')
 
 ## Invoca a opção selecionada do usuário.
-	if [[ $? -ne 0 ]]; then
-		creditos
-	fi
 
 	case $opt in 
 		1) gerdis ;;
 		2) germem ;;
 		3) gerpro ;;
-		4) leave ;;
+		4) monsis ;;
+		5) leave ;;
 	esac
 
 }
@@ -77,12 +80,14 @@ function goback(){
 	dialog --stdout --yesno 'Deseja retornar ao menu inicial?' 0 0
 
 	if [ $? = 0 ]; then
-		MAIN
+		main
 	else
 		clear
 		exit
 	fi
 }
+
+##### Gerenciar sistema #####
 
 ###  Função de gerenciamento de disco  ###
 function gerdis() {
@@ -102,7 +107,7 @@ opt=$(dialog							\
 	case $opt in
 		1) subgerdis1 ;;
 		2) subgerdis2 ;;
-		3) MAIN ;;
+		3) main ;;
 		4) leave ;;
 	esac
 }
@@ -112,7 +117,7 @@ opt=$(dialog							\
 	df -hT > /tmp/disco.txt
 	dialog --title 'Informações sobre espaço em disco' --textbox /tmp/disco.txt 0 0 \
 
-	goback
+	gerdis
 
 	}
 
@@ -123,7 +128,7 @@ opt=$(dialog							\
 	fdisk -l > /tmp/disco1.txt
 	dialog --title 'Informações sobre as partições' --textbox /tmp/disco1.txt 0 0\
 
-	goback
+	gerdis
 
 	}
 
@@ -143,7 +148,7 @@ opt=$(dialog							\
 	
 	case $opt in
 		1) subgermem1 ;;
-		2) MAIN ;;
+		2) main ;;
 		3) leave ;;
 	esac
 }
@@ -154,7 +159,7 @@ opt=$(dialog							\
 	export NMON=m
 	nmon
 
-	goback
+	germem
 
 	}
 
@@ -178,9 +183,9 @@ opt=$(dialog							\
 		1) subgerpro1 ;;
 		2) subgerpro2 ;;
 		3) subgerpro3 ;;
-		4) MAIN ;;
+		4) main ;;
 		5) leave ;;
-		*) echo "Opção inválida, tente novamente." ; sleep 2 ; gerpro ;;
+		*) main ;;
 	esac
 }
 
@@ -197,13 +202,9 @@ opt=$(dialog							\
 
 ## Pergunta ao usuário se quer voltar ou sair.
 
-	goback
+	gerpro
 
 	}
-function creditos() {
-	dialog --title 'Integrantes' --msgbox ' João Paulo\n João Ricardo\n Lucas Prudêncio\n Vitor Alves' 8 30
-goback
-}
 ## Subfunção 2 (Gerenciamento de Processos): snapshot sobre o uso da memória em tempo real
 
 	function subgerpro2() {
@@ -218,7 +219,7 @@ goback
 
 ## Pergunta ao usuário se quer voltar ou sair.
 
-	goback
+	gerpro
 
 	}
 
@@ -267,6 +268,8 @@ goback
 							--title 'FEITO'							\
 							--msgbox "Processo '$proc' de pid '$pidnum' foi encerrado."	\
 							5 60
+					else
+						gerpro
 					fi
 				else
 					exist=1	
@@ -324,7 +327,33 @@ goback
 	fi
 ## Pergunta ao usuário se quer voltar ou sair.
 
-	goback
+	gerpro
 
 	}
-MAIN
+
+##### Monitorar sistema #####
+
+function monsis() {
+
+	## Apresenta opções de monitoramento ao usuário.
+	opt=$(dialog								\
+		--stdout						\
+		--title 'MONITORAR SISTEMA'				\
+		--checklist 'Digite as opções a serem monitoradas:'	\
+		0 0 0							\
+		c	'CPU'	off					\
+		d	'Disco'	off					\
+		m	'Memória'	off				\
+		n	'Rede'	off					\
+		t	'Processos' off)
+
+	## Concatena as opções do usuário e executa o nmon.
+	
+	if [[ -n $opt ]]; then
+		export NMON="$opt"
+		nmon
+	fi	
+
+	main
+}
+main
